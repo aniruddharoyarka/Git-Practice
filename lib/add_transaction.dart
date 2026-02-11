23b74b503ed27e7afada4b0990aa5abb75e1826f
Aniruddha Roy Arka <aniruddharoyarka2@gmail.com>
2026-02-12 02:11:23 +0600

    added appbar

--- lib/add_transaction.dart
+++ lib/add_transaction.dart
@@ -6,7 +6,12 @@ class AddTransaction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Add Transaction'),
+      ),
+      body: Center(
+        child: Text('Add Transaction Page'),
+      ),
     );
   }
 }
