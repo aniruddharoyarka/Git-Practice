ab1edf2cd9fe6502f134dc86c0b516de8b169881
Aniruddha Roy Arka <aniruddharoyarka2@gmail.com>
2026-02-12 02:09:24 +0600

    added transaction page

---
+++ lib/add_transaction.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddTransaction extends StatelessWidget {
+  const AddTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
